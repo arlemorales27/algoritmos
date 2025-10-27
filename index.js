0887f87918e44958f21c2d9148fa0b6d29bd1533
arlemorales27 <amorales18@cue.edu.co>
2025-10-27 06:53:05 -0500

    9t

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+class Empleado {
+    constructor(nombre, salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
 
+    obtenerDetalles() {
+        return `Empleado: ${this.nombre}, Salario: $${this.salario}`;
+    }
+}
+
+class Gerente extends Empleado {
+    constructor(nombre, salario, departamento) {
+        super(nombre, salario);
+        this.departamento = departamento;
+    }
+}
+
+const gerente = new Gerente("Ana", 80000, "Ventas");
+console.log(gerente.obtenerDetalles());
